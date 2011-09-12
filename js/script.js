270e9b3d091e640a6d4dd85f6e92ddf168af146e
Dominik Kukacka <dominik.kukacka@gmail.com>
2011-09-12 18:58:05 +0200

    draw()

--- js/script.js
+++ js/script.js
@@ -12,16 +12,42 @@ window.requestAnimFrame = (function(){
 
 function GameEngine() {
 
-    this.entities       = [];
-    this.ctx            = null;
-    this.canvasWidth    = null;
-    this.canvasHeight   = null;    
+    this.entities           = [];
+    this.ctx                = null;
+    this.canvasWidth        = null;
+    this.canvasHeight       = null;    
+    this.canvasHalfWidth    = null;
+    this.canvasHalfHeight   = null;
 
 }
 
 
 GameEngine.prototype.init = function(ctx) {
     this.ctx = ctx;
-    this.canvasWdith = ctx.canvas.width;
-    this.canvasHeight = ctx.canvas.height;
+
+    this.canvasWidth        = ctx.canvas.width;
+    this.canvasHalfWidth    = ctx.canvas.width / 2;
+    this.canvasHeight       = ctx.canvas.height;
+    this.canvasHalfHeight   = ctx.canvas.height / 2;
+}
+
+GameEngine.prototype.addEntity = function(e) {
+    this.entities.push(e);
+}
+
+GameEngine.prototype.prototype.draw = function(drawCallback) {
+    
+    this.ctx.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.ctx.save();
+    this.ctx.translate(this.canvasHalfWidth, this.canvasHalfHeight);
+
+    var cnt = this.entities.sizeof;
+    for(var i = 0; i < cnt; i++) {
+        this.entities[i].draw(ctx);
+    }
+
+    if(drawCallback) {
+        drawCallback(this);
+    }
+
 }
